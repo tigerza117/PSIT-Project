59c904a44a152521a70075effc37cb8db87e5090
tigerza117 <noppawanza1171@gmail.com>
2020-12-07 03:27:29 +0700

    feat(routers): add authrouter controller

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -10,7 +10,7 @@ import Admin from "./views/Admin.vue";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   routes: [
     {
@@ -40,6 +40,42 @@ export default new Router({
     {
       path: "/admin",
       component: Admin,
+      meta: {
+        requiresAuth: true,
+      },
     },
   ],
 });
+
+//AuthRouter controller
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (localStorage.getItem("jwt") == null) {
+      next({
+        path: "/login",
+        params: { nextUrl: to.fullPath },
+      });
+    } else {
+      let user = JSON.parse(localStorage.getItem("user"));
+      if (to.matched.some((record) => record.meta.is_admin)) {
+        if (user.is_admin == 1) {
+          next();
+        } else {
+          next({ name: "userboard" });
+        }
+      } else {
+        next();
+      }
+    }
+  } else if (to.matched.some((record) => record.meta.guest)) {
+    if (localStorage.getItem("jwt") == null) {
+      next();
+    } else {
+      next({ name: "userboard" });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
