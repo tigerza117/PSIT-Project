e26c4172368626cfea9a4a64007f58e054575c03
tigerza117 <noppawanza1171@gmail.com>
2020-12-07 02:26:12 +0700

    feat(routes): add component to routes

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
+import Register from "./views/Register.vue";
+import Merchant from "./views/Merchant.vue";
 
 Vue.use(Router);
 
@@ -13,15 +16,15 @@ export default new Router({
     },
     {
       path: "/login",
-      component: Home,
+      component: Login,
     },
     {
       path: "/register",
-      component: Home,
+      component: Register,
     },
     {
       path: "/merchant",
-      component: Home,
+      component: Merchant,
     },
     {
       path: "/order",
