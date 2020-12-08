8d19fb563a8c34da0c32bbf3a81c6151c3060b22
tigerza117 <noppawanza1171@gmail.com>
2020-12-08 21:36:20 +0700

    refactor(backend): inital project structure

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
-    return "This is home page "
+    return "This is home page"
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug=True)
