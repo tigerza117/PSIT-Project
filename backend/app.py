29baa006d0e361c34a1059272132b8be0e9715b9
Trda092 <63070083@kmitl.ac.th>
2020-12-08 23:34:42 +0700

    build: renew to funtional structure

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,27 @@
-from flask import Flask, request
+from flask import Flask, render_template, g, jsonify
+from flask_cors import CORS
+from auth import auth
+#routes
 app = Flask(__name__)
+app.register_blueprint(auth)
+CORS(app)
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="103.91.205.130",
+  user="trda",
+  password="kkgFOpODYIXc1q2S",
+  database="trda"
+)
+
+@app.before_first_request
+def before_first_request():
+    # configure the connection pool in the global object
+    g.cnx_pool = mydb
 
 @app.route('/')
 def home():
     return "This is home page"
 
-app.run(debug=True)
+if __name__ == '__main__':
+  
+  app.run(debug=True)
