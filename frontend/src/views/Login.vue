23ce38b211a260cf5acd8b97e00c32c610e3bbd6
goteninwza007 <63070040@kmitl.ac.th>
2020-12-09 23:25:58 +0700

    feat(frontend/auth): add login page

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    This is login page
-  </div>
+  <div class="w-full max-w-md m-auto bg-white rounded-xl p-10 mt-24">
+            <p class="mb-2 text-2xl text-gray-600">Sing In</p>
+            <div class="mb-3">
+              <input class="appearance-none border border-blue-100 rounded-lg w-full py-2 px-3 text-grey-darker mt-5" id="Email" type="text" placeholder="Email">
+            </div>
+            <div class="mb-3">
+              <input class="appearance-none border border-blue-100 rounded-lg w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="Password">
+            </div>
+            <div class="flex items-center justify-between mb-4">
+              <button class="w-full bg-blue-500 hover:bg-blue-dark text-white text-xs font-bold py-3 rounded-lg" type="button">
+                Sign Up
+              </button>
+            </div>
+            <div class="mb-4">
+                <p class="text-sm text-center text-gray-600">or sign in with</p>
+            </div>
+            <button class="w-full bg-gray-50 hover:bg-blue-dark text-gray-600 text-xs font-bold py-2 rounded-lg" type="button">
+                GOOGLE
+              </button>
+        </div>
 </template>
