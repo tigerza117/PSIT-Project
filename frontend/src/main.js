69181bec096e8f8cb3ac392f2d378eecbc04ed29
tigerza117 <noppawanza1171@gmail.com>
2020-12-10 03:04:00 +0700

    feat(frontend): add baseURL to ez implement api

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import './index.css'
 import router from './router'
-import Axios from 'axios'
+//import Axios from 'axios'
 import Vuex from 'vuex'
+import Axios from 'axios'
 
 Vue.use(Vuex)
+
+Axios.defaults.baseURL = 'http://localhost:5000/'
 
 Vue.prototype.$http = Axios
 
