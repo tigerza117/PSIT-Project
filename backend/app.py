8e04cf4911fba365956ae3d3c77d491ffc3333e2
Trda092 <63070083@kmitl.ac.th>
2020-12-09 19:16:31 +0700

    test(backend/testdb): test add data base and running data on database

--- backend/app.py
+++ backend/app.py
@@ -4,13 +4,14 @@ from pkg import routes
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "hello, world!"
+
+
 with app.app_context():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://salmon:_-.*<:e5w`DqqLJW@103.91.205.130/salmon'
     app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
     CORS(app)
     routes.init_app(app)
-    app.run(debug=True)
-
-@app.route('/')
-def home():
-    return "hello, world!"
+app.run(debug=True)
