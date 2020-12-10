2dc767eb1749e943f8e5afb378670d02004733f4
Trda092 <63070083@kmitl.ac.th>
2020-12-11 02:52:49 +0700

    feat(/backend/shop): renew path /shops/<id>

--- backend/pkg/routes/shop.py
+++ backend/pkg/routes/shop.py
@@ -2,7 +2,8 @@ from inspect import Parameter
 from flask.globals import request
 from flask import jsonify
 from ..models.shop import Shop
-# from ..models.menu import Menu
+from ..models.menu import Menu
+from ..models.order import Order
 from ..models import db
 from . import app, required_params, private
 import hashlib
@@ -23,5 +24,14 @@ def put_shops(data):
 @private()
 def get_inshops(data, id):
     shop = Shop.query.filter_by(id=id).first()
-    return shop.get('id', 'name')
+    menus = Menu.query.filter_by(shop_id=id)
+    order = Order.query.filter_by(shop_id=id, status='waiting').first()
+    queue = {'queue': 'close'}
+    if order != None: 
+        queue = order.get('queue')
+    result = shop.get('name')
+    result.update({'menus': [i.get('name', 'price', 'extra_price', 'category') for i in menus]})
+    result.update(queue)
+    # print(result)
+    return result
 
