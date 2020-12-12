62d6ae3ab2efbcd8b3115a1d1aa1a7449763b475
tigerza117 <noppawanza1171@gmail.com>
2020-12-12 18:04:53 +0700

    refactor(backend/routes): refactor format use better particle code

--- backend/pkg/routes/order.py
+++ backend/pkg/routes/order.py
@@ -1,7 +1,10 @@
+from datetime import date
 from flask import jsonify
 from ..models.order import Order
 from ..models import db
-from . import app, private
+from . import app, private, required_params
+from sqlalchemy import func
+from flask.globals import request
 
 @app.route('/orders', methods=['GET'])
 @private()
@@ -10,8 +13,30 @@ def orders(data):
     orderds = Order().query.all()
     return jsonify([i.getData() for i in orderds])
 
-@app.route('/orders', methods=['PUT'])
+@app.route('/shops/<id>', methods=['PUT'])
+@required_params({"note": str})
 @private()
-def put_order(data):
-    print(data)
-    return {'success': True}, 201
+def add_order(data, id):
+    userID = data["id"]
+    body = request.get_json()
+    have_order = Order.query.filter(func.DATE(Order.created_at) == date.today(), Order.shop_id==id, Order.customer_id==userID).count()
+    print(have_order)
+    new_order = Order(
+        customer_id=userID,
+        shop_id=id, note=body["note"],
+        queue='A%02d' % Order.query.filter(func.DATE(Order.created_at) == date.today()).count()
+    )
+    db.session.add(new_order)
+    db.session.commit()
+    return {'success': True, 'order': new_order.getData()}, 201
+
+@app.route('/shops/<id>/order', methods=['GET'])
+@private()
+def user_order(data, id):
+    # shop = Shop.query.filter_by(id=id).first()
+    userID = data["id"]
+    order = Order.query.filter(func.DATE(Order.created_at) == date.today(), Order.shop_id==id, Order.customer_id==userID).first()
+    if order:
+        return {'success': True, 'order': order.getData()}, 200
+    else:
+        return 'your order not found'
--- backend/pkg/routes/shop.py
+++ backend/pkg/routes/shop.py
@@ -1,6 +1,3 @@
-from datetime import date
-from inspect import Parameter
-from os import stat_result
 from flask.globals import request
 from flask import jsonify
 from ..models.shop import Shop
@@ -11,8 +8,6 @@ from datetime import date
 import mysql.connector
 from ..models import db
 from . import app, required_params, private
-import hashlib
-import jwt
 
 @app.route('/shops', methods=['GET'])
 @private()
@@ -32,72 +27,13 @@ def get_inshops(data, id):
     menus = Menu.query.filter_by(shop_id=id)
     order = Order.query.filter_by(shop_id=id, status='waiting').first()
     queue = {'queue': 'close'}
-    if order != None: 
+    if order != None:
         queue = order.get('queue')
     result = shop.get('name')
     result.update({'menus': [i.get('name', 'price', 'extra_price', 'category') for i in menus]})
     result.update(queue)
     # print(result)
     return result
-
-@app.route('/shops/<id>', methods=['PUT'])
-@required_params({"note": str})
-@private()
-def put_inshops(data, id):
-    userID = data["id"]
-    print(data)
-    mydb = mysql.connector.connect(
-    host="103.91.205.130",
-    user="salmon",
-    password="_-.*<:e5w`DqqLJW",
-    database="salmon"
-    )
-    mycursor = mydb.cursor()
-    body = request.get_json()
-    order = Order()
-    body.update({"customer_id": userID})
-    body.update({"shop_id": id})
-    order.customer_id = body["customer_id"]
-    order.shop_id = body["shop_id"]
-    order.note = body["note"]
-    mycursor.execute("SELECT * FROM orders WHERE DATE(created_at) = DATE(NOW()) and customer_id = %s and shop_id = %s" %(userID,id))
-    myresult = mycursor.fetchall()
-    print(len(myresult))
-    if len(myresult) == 0:
-        """queue path"""
-        mycursor.execute("SELECT * FROM orders WHERE DATE(created_at) = DATE(NOW())")
-        result = mycursor.fetchall()
-        body.update({"queue": 'A'+str(len(result))})
-        order.queue = body["queue"]
-        """queue path"""
-    else:
-        return 'You have order now'
-    order.status = 'ordering'
-    db.session.add(order)
-    db.session.commit()
-    return {'success': True}, 201
-
-@app.route('/shops/<id>/order', methods=['GET'])
-@private()
-def user_orderonshop(data, id):
-    # shop = Shop.query.filter_by(id=id).first()
-    userID = data["id"]
-    mydb = mysql.connector.connect(
-    host="103.91.205.130",
-    user="salmon",
-    password="_-.*<:e5w`DqqLJW",
-    database="salmon"
-    )
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT queue, note FROM orders WHERE DATE(created_at) = DATE(NOW()) and customer_id = %s and shop_id = %s" %(userID,id))
-    myresult = mycursor.fetchall()
-    if myresult != None:
-        lst = [i for i in myresult]
-        print(lst)
-        dicts = {'queue': lst[0][0], 'note': lst[0][1]}
-        return dicts
-    else:
-        return 'your order not found'
 
 @app.route('/shops/create', methods=['PUT'])
 @required_params({"name": str, "description": str, "img": str})
