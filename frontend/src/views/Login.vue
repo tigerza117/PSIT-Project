5466f051f74f58dafaa5080935215d0eac6c3ac3
tigerza117 <noppawanza1171@gmail.com>
2020-12-12 17:52:33 +0700

    refactor(frontned/views): refactor format

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,49 +1,52 @@
 <template>
-<div class="max-w-md mx-auto my-20 shadow-xl rounded-2xl">
-  <div class="p-5 md:bg-white-100 rounded-xl">
-    <p class="topic">Sign In</p>
-    <form v-on:submit.prevent="login" class="pt-6 space-y-4 text-center">
-      <div class="mb-3">
-        <input
-          class="input"
-          id="email"
-          name="email"
-          type="text"
-          autocomplete="email"
-          placeholder="Email"
-          v-model="auth.email"
-        />
-      </div>
-      <div class="mb-3">
-        <input
-          class="input"
-          id="password"
-          name="password"
-          type="password"
-          autocomplete="current-password"
-          placeholder="Password"
-          v-model="auth.password"
-        />
-      </div>
-      <div class="flex items-center justify-between mb-4">
-        <button class="btn" type="submit">
-          Sign In
+  <div class="max-w-md mx-auto my-20 shadow-xl rounded-2xl">
+    <div class="p-5 md:bg-white-100 rounded-xl">
+      <p class="topic">Sign In</p>
+      <form v-on:submit.prevent="login" class="pt-6 space-y-4 text-center">
+        <div class="mb-3">
+          <input
+            class="input"
+            id="email"
+            name="email"
+            type="text"
+            autocomplete="email"
+            placeholder="Email"
+            v-model="auth.email"
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            class="input"
+            id="password"
+            name="password"
+            type="password"
+            autocomplete="current-password"
+            placeholder="Password"
+            v-model="auth.password"
+          />
+        </div>
+        <div class="flex items-center justify-between mb-4">
+          <button class="btn" type="submit">
+            Sign In
+          </button>
+        </div>
+        <div class="mb-4">
+          <p class="text-sm text-center text-gray-600">or sign in with</p>
+        </div>
+        <button class="btn-gray">
+          GOOGLE
         </button>
-      </div>
-      <div class="mb-4">
-        <p class="text-sm text-center text-gray-600">or sign in with</p>
-      </div>
-      <button class="btn-gray">
-        GOOGLE
-      </button>
-    </form>
-    <div class="mt-6">
+      </form>
+      <div class="mt-6">
         <p class="text-sm text-center text-gray-600">
-          Don't have an account yet? <a href="#" class="underline text-blue-400 hover:text-blue-800">Sign up</a>
+          Don't have an account yet?
+          <a href="#" class="text-blue-400 underline hover:text-blue-800"
+            >Sign up</a
+          >
         </p>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
