76f11df0ea3727eecd2c33deac98c85738dd2996
tigerza117 <noppawanza1171@gmail.com>
2020-12-14 05:11:07 +0700

    refactor(backend): remove useless import

--- backend/pkg/routes/__init__.py
+++ backend/pkg/routes/__init__.py
@@ -69,8 +69,7 @@ def required_params(required):
 
 
 def init_app(app: Flask):
-    from . import auth, user, order, shop
-    from . import menu
+    from . import auth, user, order, shop, menu
     app.register_blueprint(auth.app)
     app.register_blueprint(user.app)
     app.register_blueprint(order.app)
