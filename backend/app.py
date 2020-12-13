eed6feabb0ca012494528cc20d1320e2f79fd00a
tigerza117 <noppawanza1171@gmail.com>
2020-12-14 05:11:54 +0700

    style(backend): respone thai lang

--- backend/app.py
+++ backend/app.py
@@ -3,15 +3,15 @@ from flask_cors import CORS
 from pkg import routes
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
     return "hello, world!"
 
-
 with app.app_context():
+
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://salmon:_-.*<:e5w`DqqLJW@103.91.205.130/salmon'
     app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-    CORS(app)
     routes.init_app(app)
 app.run(debug=True, host='0.0.0.0')
